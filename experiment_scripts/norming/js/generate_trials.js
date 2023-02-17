116285a5741cc738d1b96a37b0f7bfb4d64b358b
mzettersten <mzettersten@users.noreply.github.com>
2023-02-16 19:18:05 -0500

    updates to norming experiment

--- experiment_scripts/norming/js/generate_trials.js
+++ experiment_scripts/norming/js/generate_trials.js
@@ -26,6 +26,98 @@ function generate_images() {
 
 }
 
+function generate_trial(target,foil,choices,audio_feedback = false,blank_image="stimuli/white_square.png") {
+
+	var trial_sequence = [];
+
+	var image_options = ['<img src="stimuli/'+choices[0]+'.png" style="width: 200px;height: 200px;" >', '<img src="stimuli/'+choices[1]+'.png" style="width: 200px;height: 200px;">'];
+	var stimulus= '<img src="stimuli/'+target+'.png" style="border: 5px solid #555;width: 200px;height: 200px;">';
+	var blank_stimulus= '<img src="'+blank_image+'" style="border: 5px solid #555;width: 200px;height: 200px;">';
+
+
+	var standard_trial = {
+	    type: 'html-button-response',
+	    //stimulus: cur_stimulus,
+	    stimulus: blank_stimulus,
+	    choices: ['<img src="stimuli/white_square.png">', '<img src="stimuli/white_square.png">'],
+		  prompt: "",
+		  margin_horizontal: "50px",
+		  margin_vertical: "30px",
+	    post_trial_gap: 0,
+		  trial_duration: 1000,
+		  response_ends_trials: false,
+	      data: {
+	        "target": target,
+	        "foil": foil,
+	        "options": choices
+	      },
+	  }
+	  trial_sequence.push(standard_trial);
+  
+	  var response_trial = {
+	    type: 'html-button-response',
+	    stimulus: stimulus,
+	    choices: image_options,
+		  prompt: "",
+		  margin_horizontal: "50px",
+		  margin_vertical: "30px",
+	    post_trial_gap: 0,
+	      data: {
+	        "target": target,
+	        "foil": foil,
+	        "options": choices
+	      },
+	      on_finish: function(data){
+	      	console.log(data.rt);
+	      	console.log(data.response);
+	      	console.log(data.target);
+	      	console.log(data.options);
+	      // Score the response as correct or incorrect.
+	      if (data.options[data.response] == data.target) {
+	      	data.correct = 1;
+	      } else {
+	      	data.correct = 0; 
+	      }
+	  }
+	  }
+	  trial_sequence.push(response_trial);
+
+	  if (audio_feedback) {
+	  	var audio_feedback = {
+	  	type: 'html-audio-button-response',
+	  	stimulus: function() {
+	  		last_trial_data = jsPsych.data.get().last(1).values()[0];
+	  		last_trial_correct = last_trial_data.correct
+	  		console.log(last_trial_correct);
+	  		if (last_trial_correct == 1) {
+	  			return "stimuli/bleep.m4a"
+	  		} else {
+	  			return "stimuli/buzz.m4a"
+	  		}
+	  	},
+	  	html_stimulus: stimulus,
+	    choices: image_options,
+	    margin_horizontal: "50px",
+	    margin_vertical: "30px",
+	  	trial_duration: 300,
+	  	trial_ends_after_audio: true,
+	  	response_allowed_while_playing: false,
+	  	data: {
+	        "target": target,
+	        "foil": foil,
+	        "options": choices
+	      }
+	  }
+trial_sequence.push(audio_feedback);
+
+
+	  }
+
+	  return(trial_sequence)
+
+	  
+}
+
 
 
 function generate_block() {
@@ -68,6 +160,8 @@ function generate_block() {
   var order_list_shuffled =jsPsych.randomization.shuffle(order_list);
   var all_combos_shuffled =jsPsych.randomization.shuffle(all_combos);
   console.log(order_list_shuffled);
+
+  var round_counter = 1;
 
     for (i=0; i<all_combos.length; i++) {
 	  
@@ -142,33 +236,68 @@ function generate_block() {
 	  }
 	  timeline.push(response_trial);
 
-	  var audio_feedback = {
-	  	type: 'html-audio-button-response',
-	  	stimulus: function() {
-	  		last_trial_data = jsPsych.data.get().last(1).values()[0];
-	  		last_trial_correct = last_trial_data.correct
-	  		console.log(last_trial_correct);
-	  		if (last_trial_correct == 1) {
-	  			return "stimuli/bleep.m4a"
-	  		} else {
-	  			return "stimuli/buzz.m4a"
-	  		}
-	  	},
-	  	html_stimulus: cur_stimulus,
-	    choices: cur_choices,
-	    margin_horizontal: "50px",
-	    margin_vertical: "30px",
-	  	trial_duration: 300,
-	  	trial_ends_after_audio: true,
-	  	response_allowed_while_playing: false,
-	  	data: {
-	        "target": cur_target,
-	        "foil": cur_foil,
-	        "combination_order": cur_order
-	      }
-	  }
+	  // var audio_feedback = {
+	  // 	type: 'html-audio-button-response',
+	  // 	stimulus: function() {
+	  // 		last_trial_data = jsPsych.data.get().last(1).values()[0];
+	  // 		last_trial_correct = last_trial_data.correct
+	  // 		console.log(last_trial_correct);
+	  // 		if (last_trial_correct == 1) {
+	  // 			return "stimuli/bleep.m4a"
+	  // 		} else {
+	  // 			return "stimuli/buzz.m4a"
+	  // 		}
+	  // 	},
+	  // 	html_stimulus: cur_stimulus,
+	  //   choices: cur_choices,
+	  //   margin_horizontal: "50px",
+	  //   margin_vertical: "30px",
+	  // 	trial_duration: 300,
+	  // 	trial_ends_after_audio: true,
+	  // 	response_allowed_while_playing: false,
+	  // 	data: {
+	  //       "target": cur_target,
+	  //       "foil": cur_foil,
+	  //       "combination_order": cur_order
+	  //     }
+	  // }
 
-	  timeline.push(audio_feedback);
+	  // timeline.push(audio_feedback);
+
+	  //break every 10 trials 
+	  if (i+1==60) {
+	  	var finish_button = {
+        type: 'html-audio-button-response',
+        stimulus: 'stimuli/tada.m4a',
+        html_stimulus: "<div class='row'><img src='stimuli/round_6.png' width='400'></div><div class='row'><img src='stimuli/fireworks.png' width='300'></div>",
+        prompt: "You're all done!",
+        choices: ["NEXT"],
+        trial_ends_after_audio: false,
+        response_allowed_while_playing: false
+      }
+      timeline.push(finish_button);
+
+	  } else if ((i+1)%10 == 0) {
+	  	var block_end_button = {
+        type: 'html-audio-button-response',
+        stimulus: 'stimuli/tada.m4a',
+        html_stimulus: "<div class='row'><img src='stimuli/round_"+round_counter+".png' width='400'></div><div class='row'><img src='stimuli/fireworks.png' width='300'></div>",
+        prompt: "Great job!",
+        choices: ["NEXT"],
+        trial_ends_after_audio: false,
+        response_allowed_while_playing: true
+      }
+      timeline.push(block_end_button);
+
+      var test_button = {
+      	type: 'html-button-response',
+      	stimulus: "",
+      	choices: ["<img src='stimuli/twohands.png' width='300'>"]
+      }
+      timeline.push(test_button);
+      round_counter = round_counter+1;
+
+	  }
   }
 
   return(timeline)
